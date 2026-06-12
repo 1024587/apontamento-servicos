849de650a458fbb2f9f4925233e55cf1b36dd7e9
Douglas Baldessar <aguiatec@gmail.com>
2026-06-12 13:07:36 -0300

    Integra Supabase

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from supabase import create_client
+import os
 
 app = FastAPI()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(url, key)
 
 @app.get("/")
 def home():
@@ -8,3 +15,8 @@ def home():
         "status": "online",
         "sistema": "apontamento-servicos"
     }
+
+@app.get("/funcionarios")
+def funcionarios():
+    resultado = supabase.table("funcionarios").select("*").execute()
+    return resultado.data
