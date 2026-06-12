a62e4a44400482cb1f1da881459960fc4c6dadaf
Douglas Baldessar <aguiatec@gmail.com>
2026-06-12 14:47:09 -0300

    Cria endpoint entrada

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from supabase import create_client
+from datetime import datetime
 import os
 
 app = FastAPI()
@@ -20,3 +21,30 @@ def home():
 def funcionarios():
     resultado = supabase.table("Funcionarios").select("*").execute()
     return resultado.data
+
+@app.post("/entrada")
+def registrar_entrada(telefone: str):
+
+    funcionario = (
+        supabase
+        .table("Funcionarios")
+        .select("*")
+        .eq("telefone", telefone)
+        .execute()
+    )
+
+    if not funcionario.data:
+        return {"erro": "Funcionário não encontrado"}
+
+    funcionario_id = funcionario.data[0]["id"]
+
+    supabase.table("Apontamentos").insert({
+        "funcionario_id": funcionario_id,
+        "entrada": datetime.utcnow().isoformat(),
+        "status": "aberto"
+    }).execute()
+
+    return {
+        "sucesso": True,
+        "funcionario": funcionario.data[0]["nome"]
+    }
